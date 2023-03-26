b7dad21c5e282e2f55d2e02a4b11f411e885b309
mitic996 <mitic996@gmail.com>
2023-03-26 23:34:28 +0200

    Google Extension

--- index1.js
+++ index1.js
@@ -1,14 +1,22 @@
-let myLeads = ["awesomelead", "epiclead", "greatlead"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    inputEl.value = ""
+    renderLeads()
 })
 
-
-
+function renderLeads() {
+let listItems = ""
 for(let i=0;i<myLeads.length;i++) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+    listItems += `<li>
+        <a href='${myLeads[i]}' target = '_blank'>
+        ${myLeads[i]}
+        </a>
+    </li>`
+}
+ulEl.innerHTML = listItems
 }
